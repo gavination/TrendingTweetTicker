4a33ec6e3e5a0d10248b20bc2e64e79b5a83b307
Gavination <gavinbauman1322@gmail.com>
2021-01-14 03:39:07 -0500

    Formats text analytics results and emits them to Azure Table

--- functions/AnalyticsFunc/__init__.py
+++ functions/AnalyticsFunc/__init__.py
@@ -1,12 +1,16 @@
 import logging
+import json
 import azure.functions as func
 from .utils import analytics_client
 
-def main(msg: func.QueueMessage) -> None:
+def main(inMessage: func.QueueMessage, outMessage: func.Out[str]):
     logging.info('Python queue trigger function processed a queue item: %s',
-                 msg.get_body().decode('utf-8'))
+                 inMessage.get_body().decode('utf-8'))
     
-    fetched_message = msg.get_body().decode('utf-8')
+    fetched_message = inMessage.get_body().decode('utf-8')
     client = analytics_client.authenticate_client()
 
-    analytics_client.analyze_statement(fetched_message, client)
+    processed_data = analytics_client.analyze_statement(fetched_message, client)
+
+    proc = str(processed_data)
+    outMessage.set(proc)
--- functions/AnalyticsFunc/utils/analytics_client.py
+++ functions/AnalyticsFunc/utils/analytics_client.py
@@ -2,6 +2,7 @@ from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 import os
 import logging
+import uuid
 
 # Creates an authenticated client to the Azure Text Analytics Service
 
@@ -12,13 +13,29 @@ def authenticate_client():
             credential=ta_credential)
     return text_analytics_client
 
-    # Message argument has to be an array to feed to the sentiment analysis api
 def analyze_statement(statement, client: TextAnalyticsClient):
+    
+    # Message argument has to be an array to feed to the sentiment analysis api
     response = client.analyze_sentiment(documents=[statement])
 
     docs = [doc for doc in response if not doc.is_error]
+
+    analyzed_data = {}
 
     logging.info("Logging the sentiment of the received statements")
     for idx, doc in enumerate(docs):
         logging.info("Document text: {}".format([statement][idx]))
         logging.info("Overall sentiment: {}".format(doc.sentiment))
+
+        logging.info("Formatting response...")
+        sentiment_values = {}
+        sentiment_values["positive"] = doc.confidence_scores.positive
+        sentiment_values["neutral"] = doc.confidence_scores.neutral
+        sentiment_values["negative"] = doc.confidence_scores.negative
+        analyzed_data["message"] = [statement][idx]
+        analyzed_data["sentiment"] = doc.sentiment
+        analyzed_data["sentimentValues"] = sentiment_values
+        analyzed_data["rowKey"] = str(uuid.uuid4())
+    
+    return analyzed_data
+
